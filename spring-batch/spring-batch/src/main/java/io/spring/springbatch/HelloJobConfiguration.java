5c4860c06a2a3b414b3ddaa50f4ab351b8cae0c0
Dahye-Lee <106331443+DahyeLee1205@users.noreply.github.com>
2024-09-03 22:45:44 +0900

    [spring-batch] JobInstance 추가

--- spring-batch/spring-batch/src/main/java/io/spring/springbatch/HelloJobConfiguration.java
+++ spring-batch/spring-batch/src/main/java/io/spring/springbatch/HelloJobConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @RequiredArgsConstructor
 @Configuration
